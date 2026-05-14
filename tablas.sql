f806c5910d520df9884006755d234ef102bc5133
anavillagomez82 <ana.villagomez@ucsp.edu.pe>
2026-05-14 09:56:47 -0500

    feat_tabla_detalle_prestamo.sql

--- tablas.sql
+++ tablas.sql
@@ -2,3 +2,14 @@ CREATE TABLE GENERO (
 id_genero INT PRIMARY KEY AUTO_INCREMENT,
 nombre_genero VARCHAR(50) NOT NULL
 );
+
+CREATE TABLE DETALLE_PRESTAMO (
+  id_detalle INT PRIMARY KEY AUTO_INCREMENT,
+  prestamo_id INT,
+  videojuego_id INT,
+  FOREIGN KEY (prestamo_id) REFERENCES PRESTAMO(id_prestamo),
+  FOREIGN KEY (videojuego_id) REFERENCES VIDEOJUEGO(id_videojuego),
+  );
+  
+  
+  

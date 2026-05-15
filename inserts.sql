d8d1151904196986b44c481338fd0bc50dc35d06
Juan Pablo Cateriano Justo <juanpablocateriano@gmail.com>
2026-05-14 20:01:02 -0500

    Create inserts.sql

---
+++ inserts.sql
@@ -0,0 +1,36 @@
+INSERT INTO ROL (nombre_rol, descripcion)
+VALUES
+('Administrador', 'Control total del sistema'),
+('Cliente', 'Usuario normal del sistema');
+
+INSERT INTO USUARIO (nombre, correo, telefono, fecha_registro, id_rol)
+VALUES
+('Juan Perez', 'juan@gmail.com', '999111222', '2025-01-10', 2),
+('Maria Lopez', 'maria@gmail.com', '988777666', '2025-02-15', 1);
+
+INSERT INTO GENERO (nombre_genero)
+VALUES
+('Acción'),
+('RPG'),
+('Terror'),
+('Aventura');
+
+INSERT INTO PLATAFORMA (nombre_plataforma)
+VALUES
+('PC'),
+('PlayStation 5'),
+('Xbox Series X'),
+('Nintendo Switch');
+
+INSERT INTO DESARROLLADOR (nombre_desarrollador)
+VALUES
+('FromSoftware'),
+('Nintendo'),
+('Rockstar Games'),
+('Valve');
+
+INSERT INTO ESTADO (tipo_estado)
+VALUES
+('Disponible'),
+('Prestado'),
+('Mantenimiento');
